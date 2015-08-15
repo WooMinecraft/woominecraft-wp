27855c2ee4c4c616bff8b29c0cddeeb802e9dee7
Jay Wood <jerry@webdevstudios.com>
2015-08-15 18:10:44 -0400

    Begin re-writes

--- functions.php
+++ functions.php
@@ -4,12 +4,73 @@
 	Plugin URI: http://plugish.com/plugins/minecraft_woo
 	Description: To be used in conjunction with the minecraft_woo plugin.  If you do not have it you can get it on the repository at <a href="https://github.com/JayWood/WooMinecraft">Github</a>.  Please be sure and fork the repository and make pull requests.
 	Author: Jerry Wood
-	Version: 0.1b
+	Version: 0.1.0
 	Author URI: http://plugish.com
 */
 
-include 'inc/admin.class.php';
-include 'inc/main.class.php';
+//include 'inc/admin.class.php';
+//include 'inc/main.class.php';
+
+
+function wmc_autoload_classes( $class_name ) {
+	if ( 0 != strpos( $class_name, 'WCM_' ) ) {
+		return false;
+	}
+
+	$filename = strtolower( str_ireplace(
+		array( 'WDSCPN_', '_' ),
+		array( '', '-' ),
+		$class_name
+	) );
+
+	WooMinecraft::include_file( $filename );
+	return true;
+}
+
+class WooMinecraft{
+
+
+	/**
+	 * Include a file from the includes directory
+	 *
+	 * @since  0.1.0
+	 * @param  string  $filename Name of the file to be included
+	 * @return bool    Result of include call.
+	 */
+	public static function include_file( $filename ) {
+		$file = self::dir( 'includes/'. $filename .'.php' );
+		if ( file_exists( $file ) ) {
+			return include_once( $file );
+		}
+		return false;
+	}
+
+	/**
+	 * This plugin's directory
+	 *
+	 * @since  0.1.0
+	 * @param  string $path (optional) appended path
+	 * @return string       Directory and path
+	 */
+	public static function dir( $path = '' ) {
+		static $dir;
+		$dir = $dir ? $dir : trailingslashit( dirname( __FILE__ ) );
+		return $dir . $path;
+	}
+
+	/**
+	 * This plugin's url
+	 *
+	 * @since  0.1.0
+	 * @param  string $path (optional) appended path
+	 * @return string       URL and path
+	 */
+	public static function url( $path = '' ) {
+		static $url;
+		$url = $url ? $url : trailingslashit( plugin_dir_url( __FILE__ ) );
+		return $url . $path;
+	}
+}
 
 function has_commands( $data ) {
 	if ( is_array( $data ) ) {
