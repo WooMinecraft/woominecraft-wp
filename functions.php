8df68030fef738c66c86b48c5bd04972184eea8a
Jay Wood <jerry@webdevstudios.com>
2015-08-15 17:20:52 -0400

    Clean up functions formatting

--- functions.php
+++ functions.php
@@ -11,26 +11,24 @@
 include 'inc/admin.class.php';
 include 'inc/main.class.php';
 
-function has_commands($data){
-	if(is_array($data)){
+function has_commands( $data ) {
+	if ( is_array( $data ) ) {
 		// Assume $data is cart contents
-		foreach($data as $item){
-			$metag = get_post_meta($item['product_id'], 'minecraft_woo_g', true);
-			$metav = get_post_meta($item['variation_id'], 'minecraft_woo_v', true);
-			if(empty($metag) && empty($metav)){
+		foreach ( $data as $item ) {
+			$metag = get_post_meta( $item['product_id'], 'minecraft_woo_g', true );
+			$metav = get_post_meta( $item['variation_id'], 'minecraft_woo_v', true );
+			if ( empty( $metag ) && empty( $metav ) ) {
 				continue;
-			}else{
+			} else {
 				return true;
 			}
 		}
-	}else{
-		
 	}
-	
+
 	return false;
 }
 
-new Woo_Minecraft_Admin;	
-register_activation_hook(__FILE__, array('Woo_Minecraft_Admin', 'install')); 
-register_uninstall_hook(__FILE__, array('Woo_Minecraft_Admin', 'uninstall'));
+new Woo_Minecraft_Admin;
+register_activation_hook( __FILE__, array( 'Woo_Minecraft_Admin', 'install' ) );
+register_uninstall_hook( __FILE__, array( 'Woo_Minecraft_Admin', 'uninstall' ) );
 new Woo_Minecraft;
