af0150400037f5faa232a4b6fd5e94cbdcbfb791
Jay Wood <jerry@webdevstudios.com>
2016-01-31 14:58:42 -0500

    Setup gulp for CSS and sass - breaks admin on purpose

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require( 'gulp' ),
 	gp_uglify = require( 'gulp-uglify' ),
-	gp_rename = require( 'gulp-rename' );
+	gp_rename = require( 'gulp-rename' ),
+	gp_sass = require( 'gulp-sass' ),
+	gp_cssmin = require( 'gulp-cssmin' );
 
 var scripts = [
 	'assets/js/jquery.woo.js'
 ];
+
+var sass_options = {
+	outputStyle: 'expanded'
+};
 
 gulp.task( 'scripts', function() {
 	gulp.src( 'assets/js/jquery.woo.js' )
@@ -13,8 +19,23 @@ gulp.task( 'scripts', function() {
 		.pipe( gulp.dest( 'assets/js' ) );
 } );
 
-gulp.task( 'watch', function() {
-	gulp.watch( 'assets/js/*.js', ['scripts'] );
+gulp.task( 'sass', function() {
+	gulp.src( 'assets/sass/*.scss' )
+		.pipe( gp_sass( sass_options ) )
+		.pipe( gp_rename( 'style.css' ) )
+		.pipe( gulp.dest( './' ) );
 } );
 
-gulp.task( 'default', ['scripts'] );
+gulp.task( 'cssmin', function() {
+	gulp.src( 'style.css' )
+		.pipe( gp_cssmin() )
+		.pipe( gp_rename( 'style.min.css' ) )
+		.pipe( gulp.dest( './' ) );
+} );
+
+gulp.task( 'watch', function() {
+	gulp.watch( 'assets/js/*.js', ['scripts'] );
+	gulp.watch( 'assets/sass/*.scss', [ 'sass', 'cssmin' ] );
+} );
+
+gulp.task( 'default', ['scripts', 'sass', 'cssmin'] );
--- includes/views/group-commands.php
+++ includes/views/group-commands.php
@@ -5,12 +5,11 @@
 		<label for="woo_minecraft_general"><?php _e( 'Commands', 'wmc' ); ?></label>
 		<input type="button" class="button button-primary woo_minecraft_add" name="Add" id="woo_minecraft_add" value="<?php _e( 'Add', 'wmc' ); ?>"/>
 		<input type="button" class="button woo_minecraft_reset" name="Reset" id="woo_minecraft_reset" value="<?php _e( 'Reset Fields', 'wmc' ); ?>"/>
-		<img class="help_tip" data-tip="<?php _e( 'Any commands added here, will run on top of variable commands if any. <br /><br />No leading slash is needed.', 'wmc' ); ?> />" src="<?php echo plugins_url( 'help.png', dirname( __FILE__ ) ); ?>" height="16" width="16"/>
-		<span class="tip-test tip tips" data-tip="This is a tip"></span>
-				<span class="woo_minecraft_copyme" style="display:none">
-					<input type="text" name="minecraft_woo[general][]" value="" class="short" placeholder="<?php _e( 'Use %s for player name', 'wmc' ); ?>"/>
-					<input type="button" class="button button-small delete remove_row" value="Delete">
-				</span>
+		<span class="woocommerce-help-tip" data-tip="<?php _e( 'Any commands added here, will run on top of variable commands if any; no leading slash is needed.', 'wmc' );  ?>"></span>
+		<span class="woo_minecraft_copyme" style="display:none">
+			<input type="text" name="minecraft_woo[general][]" value="" class="short" placeholder="<?php _e( 'Use %s for player name', 'wmc' ); ?>"/>
+			<input type="button" class="button button-small delete remove_row" value="Delete">
+		</span>
 		<?php if ( ! empty( $meta ) ) : ?>
 			<?php foreach ( $meta as $command ) : ?>
 				<span>
