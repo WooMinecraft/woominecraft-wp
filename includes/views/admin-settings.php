035359ccc69ac37734e4b70e115aa4b58ee10e21
JayWood <jay@plugish.com>
2016-07-03 20:11:52 -0400

    Use submit_button() and tab the code

--- includes/views/admin-settings.php
+++ includes/views/admin-settings.php
@@ -11,38 +11,38 @@
 				</tr>
 			</thead>
 			<tbody>
-			<tr>
-				<?php
-				$option = get_option( 'wm_servers', array() );
-				if ( empty( $option ) ) {
-					?>
-					<td>
-						<input type="text" name="wm_server[0][name]" value="<?php echo ''; ?>" />
-					</td>
-					<td>
-						<input type="text" name="wm_server[0][key]" id="wm_key" value="<?php echo get_option( 'wm_key' ); ?>"/>
-						<p class="description"><?php printf( __( 'Type %s in-game as op to get your key.', 'woominecraft' ), '/woo register' ); ?></p>
-					</td>
+				<tr>
 					<?php
-				} else {
-					$count = 0;
-					foreach ( $option as $server ) {
+					$option = get_option( 'wm_servers', array() );
+					if ( empty( $option ) ) {
 						?>
 						<td>
-							<input type="text" name="wm_server[<?php echo $count; ?>][name]" value="<?php echo esc_attr( $server['name'] ); ?>"/>
+							<input type="text" name="wm_server[0][name]" value="<?php echo ''; ?>" />
 						</td>
 						<td>
-							<input type="text" name="wm_server[<?php echo $count; ?>][key]" id="wm_key" value="<?php echo esc_attr( $server['key'] ); ?>"/>
+							<input type="text" name="wm_server[0][key]" id="wm_key" value="<?php echo get_option( 'wm_key' ); ?>"/>
 							<p class="description"><?php printf( __( 'Type %s in-game as op to get your key.', 'woominecraft' ), '/woo register' ); ?></p>
 						</td>
 						<?php
-						$count ++;
+					} else {
+						$count = 0;
+						foreach ( $option as $server ) {
+							?>
+							<td>
+								<input type="text" name="wm_server[<?php echo $count; ?>][name]" value="<?php echo esc_attr( $server['name'] ); ?>"/>
+							</td>
+							<td>
+								<input type="text" name="wm_server[<?php echo $count; ?>][key]" id="wm_key" value="<?php echo esc_attr( $server['key'] ); ?>"/>
+								<p class="description"><?php printf( __( 'Type %s in-game as op to get your key.', 'woominecraft' ), '/woo register' ); ?></p>
+							</td>
+							<?php
+							$count ++;
+						}
 					}
-				}
-				?>
-			</tr>
+					?>
+				</tr>
 			</tbody>
 		</table>
-		<?php echo get_submit_button(); ?>
+		<?php submit_button(); ?>
 	</form>
 </div>
