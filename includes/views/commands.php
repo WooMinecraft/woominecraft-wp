a1ddcf86ae5c92c372448fc4c30d5c380c5e5631
Jay Wood <jerry@webdevstudios.com>
2016-07-29 11:59:11 -0400

    Remove junky old code

--- includes/views/commands.php
+++ includes/views/commands.php
@@ -47,22 +47,4 @@ $servers = woo_minecraft()->admin->get_servers();
 			<?php endif; ?>
 		</tbody>
 	</table>
-<!--	<div class="form-fields">-->
-<!--		<div>-->
-<!--			<input type="button" class="button button-primary woo_minecraft_add" name="Add" id="woo_minecraft_add" value="--><?php //_e( 'Add', 'woominecraft' ); ?><!--"/>-->
-<!--			<input type="button" class="button woo_minecraft_reset" name="Reset" id="woo_minecraft_reset" value="--><?php //_e( 'Reset Fields', 'woominecraft' ); ?><!--"/>-->
-<!--		</div>-->
-<!--		<span class="woo_minecraft_copyme command" style="display:none">-->
-<!--			<input type="text" name="minecraft_woo[--><?php //echo $post->ID; ?><!--][]" value="" class="short" placeholder="--><?php //_e( 'Use %s for player name', 'woominecraft' ); ?><!--"/>-->
-<!--			<input type="button" class="button button-small delete remove_row" value="Delete">-->
-<!--		</span>-->
-<!--		--><?php //if ( ! empty( $meta ) ) : ?>
-<!--			--><?php //foreach ( $meta as $command ) : ?>
-<!--				<span class="command">-->
-<!--					<input type="text" name="minecraft_woo[--><?php //echo $post->ID; ?><!--][]" value="--><?php //echo $command; ?><!--" class="short"/>-->
-<!--					<input type="button" class="button button-small delete remove_row" value="--><?php //_e( 'Delete', 'woominecraft' ); ?><!--">-->
-<!--				</span>-->
-<!--			--><?php //endforeach; ?>
-<!--		--><?php //endif; ?>
-<!--	</div>-->
 </div>
