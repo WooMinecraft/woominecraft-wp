c8672943b5e642932b0a48daeb914c3c1c7f879b
Jay Wood <jerry@webdevstudios.com>
2017-04-16 13:56:44 -0400

    Update docs

--- docs/elementlist.js
+++ docs/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","WCM_WooCommerce"],["f","wmc_autoload_classes()"],["f","wmc_items_have_commands()"],["f","woo_minecraft()"],["c","WooMinecraft\\Admin\\WCM_Admin"],["c","WooMinecraft\\WooMinecraft"]];
+ApiGen.elements = [["f","wmc_autoload_classes()"],["f","wmc_items_have_commands()"],["f","woo_minecraft()"],["c","WooMinecraft\\Admin\\WCM_Admin"],["c","WooMinecraft\\WooCommerce\\WCM_WooCommerce"],["c","WooMinecraft\\WooMinecraft"]];
