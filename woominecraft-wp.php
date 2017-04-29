6be6548c9e8b95fc4b8cf1dd4b9a5e96d4d76b20
Jay Wood <jerry@webdevstudios.com>
2017-04-29 12:46:41 -0400

    Move all WooCommerce code to it's own class.

--- woominecraft-wp.php
+++ woominecraft-wp.php
@@ -161,7 +161,8 @@ class WooMinecraft {
 	 * @since  1.0.0
 	 */
 	private function plugin_classes() {
-		$this->admin = new \WooMinecraft\Admin\WCM_Admin( $this );
+		$this->admin       = new \WooMinecraft\Admin\WCM_Admin( $this );
+		$this->woocommerce = new \WooMinecraft\WooCommerce\WCM_WooCommerce( $this );
 	}
 
 	/**
@@ -183,10 +184,7 @@ class WooMinecraft {
 	 * @return void
 	 */
 	public function hooks() {
-		add_action( 'woocommerce_checkout_process', array( $this, 'check_player' ) );
-		add_action( 'woocommerce_checkout_update_order_meta', array( $this, 'save_commands_to_order' ) );
-		add_action( 'woocommerce_before_checkout_billing_form', array( $this, 'additional_checkout_field' ) );
-		add_action( 'woocommerce_thankyou', array( $this, 'thanks' ) );
+
 
 		add_action( 'template_redirect', array( $this, 'json_feed' ) );
 
@@ -230,115 +228,6 @@ class WooMinecraft {
 	}
 
 	/**
-	 * Produces the JSON Feed for Orders Pending Delivery
-	 *
-	 * @return void
-	 *
-	 * @author JayWood
-	 * @since 1.0.0
-	 */
-	public function json_feed() {
-
-		if ( ! isset( $_REQUEST['wmc_key'] ) ) {
-			// Bail if no key
-			return;
-		}
-
-		$servers = get_option( 'wm_servers', array() );
-		if ( empty( $servers ) ) {
-			wp_send_json_error( array( 'msg' => 'No servers setup, check WordPress config.' ) );
-		}
-		$keys = wp_list_pluck( $servers, 'key' );
-		if ( empty( $keys ) ) {
-			wp_send_json_error( array( 'msg' => 'WordPress keys are not set.' ) );
-		}
-
-		if ( false === array_search( $_GET['wmc_key'], $keys ) ) {
-			wp_send_json_error( array( 'msg' => 'Invalid key supplied to WordPress, compare your keys.' ) );
-		}
-
-		$key = esc_attr( $_GET['wmc_key'] );
-
-		if ( isset( $_REQUEST['processedOrders'] ) ) {
-
-			$this->process_completed_commands( $key );
-		}
-
-		if ( false === ( $output = get_transient( $this->command_transient ) ) || isset( $_GET['delete-trans'] ) ) {
-
-			$delivered = '_wmc_delivered_' . $key;
-			$meta_key  = '_wmc_commands_' . $key;
-
-			$order_query = apply_filters( 'woo_minecraft_json_orders_args', array(
-				'posts_per_page' => '-1',
-				'post_status'    => 'wc-completed',
-				'post_type'      => 'shop_order',
-				'meta_query'     => array(
-					'relation' => 'AND',
-					array(
-						'key'     => $meta_key,
-						'compare' => 'EXISTS',
-					),
-					array(
-						'key'     => $delivered,
-						'compare' => 'NOT EXISTS',
-					),
-				),
-			) );
-
-			$orders = get_posts( $order_query );
-
-			$output = array();
-
-			if ( ! empty( $orders ) ) {
-				foreach ( $orders as $wc_order ) {
-					if ( ! isset( $wc_order->ID ) ) {
-						continue;
-					}
-
-					$player_id   = get_post_meta( $wc_order->ID, 'player_id', true );
-					$order_array = $this->generate_order_json( $wc_order, $key );
-
-					if ( ! empty( $order_array ) ) {
-						if ( ! isset( $output[ $player_id ] ) ) {
-							$output[ $player_id ] = array();
-						}
-						$output[ $player_id ][ $wc_order->ID ] = $order_array;
-					}
-				}
-			}
-
-			set_transient( $this->command_transient, $output, 60 * 60 ); // Stores the feed in a transient for 1 hour.
-		} // End if().
-
-		wp_send_json_success( $output );
-
-	}
-
-	/**
-	 * Generates the order JSON data for a single order.
-	 *
-	 * @param \WP_Post $order_post The post to get the commands from.
-	 * @param string  $key        The server key to pluck orders with.
-	 *
-	 * @return array|mixed
-	 *
-	 * @author JayWood
-	 * @since  1.0.0
-	 */
-	private function generate_order_json( \WP_Post $order_post, $key ) {
-
-		if ( ! isset( $order_post->ID ) ) {
-			return array();
-		}
-
-		$general_commands = get_post_meta( $order_post->ID, '_wmc_commands_' . $key, true );
-
-		return $general_commands;
-	}
-
-
-	/**
 	 * Setup Localization
 	 *
 	 * @return void
@@ -363,7 +252,7 @@ class WooMinecraft {
 	 * @author JayWood
 	 * @since 1.0.0
 	 */
-	private function sanitized_orders_post( $post_data ) {
+	public function sanitized_orders_post( $post_data ) {
 		$decoded = json_decode( stripslashes( urldecode( $post_data ) ) );
 		if ( empty( $decoded ) ) {
 			return array();
@@ -389,84 +278,6 @@ class WooMinecraft {
 		}
 
 		delete_transient( $this->command_transient );
-	}
-
-	/**
-	 * Processes all completed commands.
-	 *
-	 * @param string $key The server key to check against.
-	 *
-	 * @return void
-	 *
-	 * @author JayWood
-	 * @since 1.0.0
-	 */
-	private function process_completed_commands( $key = '' ) {
-		$delivered = '_wmc_delivered_' . $key;
-		$order_ids = (array) $this->sanitized_orders_post( $_POST['processedOrders'] );
-
-		if ( empty( $order_ids ) ) {
-			wp_send_json_error( array( 'msg' => __( 'Commands was empty', 'woominecraft' ) ) );
-		}
-
-		// Set the orders to delivered
-		foreach ( $order_ids as $order_id ) {
-			update_post_meta( $order_id, $delivered, true );
-		}
-
-		$this->bust_command_cache();
-	}
-
-
-	/**
-	 * Adds a field to the checkout form, requiring the user to enter their Minecraft Name
-	 *
-	 * @param object $cart WooCommerce Cart Object.
-	 *
-	 * @return boolean  False on failure, true otherwise.
-	 *
-	 * @TODO If $cart is passed into this function, why access the $woocommerce global at all???
-	 *
-	 * @author JayWood
-	 * @since 1.0.0
-	 */
-	public function additional_checkout_field( $cart ) {
-		global $woocommerce;
-
-		$items = $woocommerce->cart->cart_contents;
-		if ( ! wmc_items_have_commands( $items ) || ! function_exists( 'woocommerce_form_field' ) ) {
-			return false;
-		}
-
-		?>
-		<div id="woo_minecraft"><?php
-		woocommerce_form_field( 'player_id', array(
-			'type'        => 'text',
-			'class'       => array(),
-			'label'       => __( 'Player ID ( Minecraft Username ):', 'woominecraft' ),
-			'placeholder' => __( 'Required Field', 'woominecraft' ),
-		), $cart->get_value( 'player_id' ) );
-		?></div><?php
-
-		return true;
-	}
-
-	/**
-	 * Resets an order from being delivered.
-	 *
-	 * @param integer $order_id   The order ID.
-	 * @param string  $server_key The server key.
-	 *
-	 * @return boolean
-	 *
-	 * @author JayWood
-	 * @since 1.0.0
-	 */
-	public function reset_order( $order_id, $server_key ) {
-		delete_post_meta( $order_id, '_wmc_delivered_' . $server_key );
-		$this->bust_command_cache( $order_id );
-
-		return true;
 	}
 
 	/**
@@ -519,132 +330,6 @@ class WooMinecraft {
 		}
 
 		return $mc_json;
-	}
-
-	/**
-	 * Checks if Minecraft Username is valid
-	 *
-	 * @return void
-	 *
-	 * @TODO Use the default WC() function instead of grabbing the global here.
-	 *
-	 * @author JayWood
-	 * @since 1.0.0
-	 */
-	public function check_player() {
-		global $woocommerce;
-
-		if ( ! $woocommerce instanceof \WooCommerce ) {
-			return;
-		}
-
-		$player_id = isset( $_POST['player_id'] ) ? esc_attr( $_POST['player_id'] ) : false;
-		$items     = $woocommerce->cart->cart_contents;
-
-		if ( ! wmc_items_have_commands( $items ) ) {
-			return;
-		}
-
-		if ( ! $player_id ) {
-			wc_add_notice( __( 'You MUST provide a Minecraft username.', 'woominecraft' ), 'error' );
-
-			return;
-		}
-
-		// Grab JSON data
-		$mc_json = $this->mojang_player_cache( $player_id );
-		if ( ! $mc_json ) {
-			wc_add_notice( __( 'We cannot retrieve your account from the Mojang API. Try again later, or contact an administrator.', 'woominecraft' ), 'error' );
-		}
-
-		if ( isset( $mc_json->demo ) ) {
-			wc_add_notice( __( 'We do not allow unpaid-accounts to make donations, sorry!', 'woominecraft' ), 'error' );
-
-			return;
-		}
-	}
-
-	/**
-	 * Updates an order's meta data with the commands hash.
-	 *
-	 * @param integer $order_id The order to save command data to.
-	 *
-	 * @return void
-	 *
-	 * @author JayWood
-	 * @since 1.0.0
-	 *
-	 * @TODO Revisit this function and optimize it.
-	 */
-	public function save_commands_to_order( $order_id ) {
-		$order_data = new \WC_Order( $order_id );
-		$items      = $order_data->get_items();
-		$tmp_array  = array();
-
-		if ( ! isset( $_POST['player_id'] ) || empty( $_POST['player_id'] ) ) {
-			return;
-		}
-
-		$player_name = esc_attr( $_POST['player_id'] );
-		update_post_meta( $order_id, 'player_id', $player_name );
-
-		foreach ( $items as $item ) {
-
-			// If this is a variable product, use that meta, otherwise check for the product data and use it.
-			$product_id = isset( $item['variation_id'] ) && ! empty( $item['variation_id'] ) ? absint( $item['variation_id'] ) : absint( $item['product_id'] );
-			if ( empty( $product_id ) ) {
-				continue;
-			}
-
-			$item_commands = get_post_meta( $product_id, 'wmc_commands', true );
-			if ( empty( $item_commands ) ) {
-				continue;
-			}
-
-			// Loop over the command set for every 1 qty of the item.
-			$qty = absint( $item['qty'] );
-			for ( $n = 0; $n < $qty; $n ++ ) {
-				foreach ( $item_commands as $server_key => $command ) {
-					if ( ! isset( $tmp_array[ $server_key ] ) ) {
-						$tmp_array[ $server_key ] = array();
-					}
-
-					if ( is_array( $command ) ) {
-						foreach ( $command as $c ) {
-							$tmp_array[ $server_key ][] = sprintf( $c, $player_name );
-						}
-					} else {
-						$tmp_array[ $server_key ][] = sprintf( $command, $player_name );
-					}
-				}
-			}
-		}
-
-		if ( ! empty( $tmp_array ) ) {
-			foreach ( $tmp_array as $server_key => $commands ) {
-				update_post_meta( $order_id, '_wmc_commands_' . $server_key, $commands );
-			}
-		}
-	}
-
-	/**
-	 * Adds the Minecraft username to the thank you page.
-	 *
-	 * @param integer $id The order ID.
-	 *
-	 * @return void
-	 *
-	 * @author JayWood
-	 * @since  1.0.0
-	 */
-	public function thanks( $id ) {
-		$player_name = get_post_meta( $id, 'player_id', true );
-		if ( ! empty( $player_name ) ) {
-			?>
-			<div class="woo_minecraft"><h4><?php esc_attr_e( 'Minecraft Details', 'woominecraft' ); ?></h4>
-
-			<p><strong><?php esc_attr_e( 'Username:', 'woominecraft' ); ?></strong><?php echo $player_name ?></p></div><?php
-		}
 	}
 
 	/**
