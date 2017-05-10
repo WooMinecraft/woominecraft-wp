334906ed38e4340bc7735388c46ecb2ef6b57489
Jay Wood <jerry@webdevstudios.com>
2017-05-10 09:37:17 -0400

    Update message to display restriction on server keys. #37

--- includes/views/server-section.php
+++ includes/views/server-section.php
@@ -1,14 +1,17 @@
 <tr valign="top" class="woominecraft">
 	<th scope="row" class="titledesc"><?php _e( 'Available Servers', 'woominecraft' ); ?></th>
 	<td>
-		<p class="description"><?php _e( 'CAUTION: Changing your server keys will invalidate any/all orders with the previous key. You cannot undo this.', 'woominecraft' ); ?></p>
+		<p class="description">
+			<?php _e( 'CAUTION: Changing your server keys will invalidate any/all orders with the previous key. You cannot undo this.', 'woominecraft' ); ?><br />
+			<?php _e( 'Server keys are limited to alpha-numeric characters and include @, #, ! but nothing more.', 'woominecraft' ); ?>
+		</p>
 		<br /><br />
 		<table class="wc_shipping widefat" cellspacing="0">
 			<thead>
 			<tr>
 				<th class="sort"></th>
 				<th class="wmc_label"><?php _e( 'Dropdown Label', 'woominecraft' ); ?></th>
-				<th class="wmc_key"><?php _e( 'Key', 'woominecraft' ); ?></th>
+				<th class="wmc_key"><?php _e( 'Key', 'woominecraft' ); ?><? echo wc_help_tip( 'test' ); ?></th>
 				<th><input type="button" class="button button-primary wmc_add_server" value="<?php _e( 'Add Server', 'woominecraft' ); ?>" /> </th>
 			</tr>
 			</thead>
