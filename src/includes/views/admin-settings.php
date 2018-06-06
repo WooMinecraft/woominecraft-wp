f7d1685c89473ad03593f223bb635bc36bf2a9e6
JayWood <jjwood2004@gmail.com>
2018-06-05 22:42:15 -0600

    Fix some, not all, phpcs complaints

--- src/includes/views/admin-settings.php
+++ src/includes/views/admin-settings.php
@@ -1,13 +1,13 @@
 <div class="wrap">
-	<h2><?php printf( __( ' %s Options', 'woominecraft' ), 'WooMinecraft' ); ?></h2>
+	<h2><?php printf( esc_html__( ' %s Options', 'woominecraft' ), 'WooMinecraft' ); ?></h2>
 
 	<form method="post" action="options.php">
 		<?php settings_fields( 'woo_minecraft' ); ?>
 		<table class="woominecraft form-table">
 			<thead>
 				<tr>
-					<th><?php _e( 'Server Label', 'woominecraft' ); ?></th>
-					<th><?php _e( 'Server Key', 'woominecraft' ); ?></th>
+					<th><?php esc_html_e( 'Server Label', 'woominecraft' ); ?></th>
+					<th><?php esc_html_e( 'Server Key', 'woominecraft' ); ?></th>
 				</tr>
 			</thead>
 			<tbody>
@@ -20,8 +20,8 @@
 							<input type="text" name="wm_server[0][name]" value="<?php echo ''; ?>" />
 						</td>
 						<td>
-							<input type="text" name="wm_server[0][key]" id="wm_key" value="<?php echo get_option( 'wm_key' ); ?>"/>
-							<p class="description"><?php printf( __( 'Type %s in-game as op to get your key.', 'woominecraft' ), '/woo register' ); ?></p>
+							<input type="text" name="wm_server[0][key]" id="wm_key" value="<?php echo esc_attr( get_option( 'wm_key' ) ); ?>"/>
+							<p class="description"><?php printf( esc_html__( 'Type %s in-game as op to get your key.', 'woominecraft' ), '/woo register' ); ?></p>
 						</td>
 						<?php
 					} else {
@@ -29,14 +29,14 @@
 						foreach ( $option as $server ) {
 							?>
 							<td>
-								<input type="text" name="wm_server[<?php echo $count; ?>][name]" value="<?php echo esc_attr( $server['name'] ); ?>"/>
+								<input type="text" name="wm_server[<?php echo (int) $count; ?>][name]" value="<?php echo esc_attr( $server['name'] ); ?>"/>
 							</td>
 							<td>
-								<input type="text" name="wm_server[<?php echo $count; ?>][key]" id="wm_key" value="<?php echo esc_attr( $server['key'] ); ?>"/>
-								<p class="description"><?php printf( __( 'Type %s in-game as op to get your key.', 'woominecraft' ), '/woo register' ); ?></p>
+								<input type="text" name="wm_server[<?php echo (int) $count; ?>][key]" id="wm_key" value="<?php echo esc_attr( $server['key'] ); ?>"/>
+								<p class="description"><?php printf( esc_html__( 'Type %s in-game as op to get your key.', 'woominecraft' ), '/woo register' ); ?></p>
 							</td>
 							<?php
-							$count ++;
+							$count++;
 						}
 					}
 					?>
