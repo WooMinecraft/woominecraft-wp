efeaf9dde533debe0df4e5fb6ab0833037803269
JayWood <jjwood2004@gmail.com>
2018-06-05 22:11:08 -0600

    Bump version in main plugin file... derp

--- src/woominecraft.php
+++ src/woominecraft.php
@@ -4,7 +4,7 @@ Plugin Name: Minecraft WooCommerce
 Plugin URI: http://woominecraft.com
 Description: To be used in conjunction with the WooMinecraft Bukkit plugin.  If you do not have it you can get it on the repository at <a href="https://github.com/JayWood/WooMinecraft">Github</a>.  Please be sure and fork the repository and make pull requests.
 Author: Jerry Wood
-Version: 1.1.1
+Version: 1.2
 License: GPLv2
 Text Domain: woominecraft
 Domain Path: /languages
