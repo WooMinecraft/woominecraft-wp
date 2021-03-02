074b3e6c84cdbffea282d2a071631c58267e5ecf
JayWood <jjwood2004@gmail.com>
2021-03-02 14:07:32 -0500

    Remove build directory from sourcecode

--- build/settings.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '399cb73706126faa06ee058ed8ca7f5a');
