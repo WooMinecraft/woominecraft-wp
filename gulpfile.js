becc4d80fc535d778d3695ccb6fa050fc77d2011
JayWood <jjwood2004@gmail.com>
2021-12-17 14:12:58 -0500

    Remove sass crap for now

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,12 @@
 var gulp = require( 'gulp' ),
 	gp_uglify = require( 'gulp-uglify' ),
-	gp_rename = require( 'gulp-rename' ),
-	gp_sass = require( 'gulp-sass' ),
-	gp_cssmin = require( 'gulp-cssmin' );
-
-var scripts = [
-	'assets/js/jquery.woo.js'
-];
-
-var sass_options = {
-	outputStyle: 'expanded'
-};
+	gp_rename = require( 'gulp-rename' );
 
 gulp.task( 'scripts', function() {
 	return gulp.src( 'assets/js/jquery.woo.js' )
 		.pipe( gp_uglify() )
 		.pipe( gp_rename( 'jquery.woo.min.js' ) )
 		.pipe( gulp.dest( 'assets/js' ) );
-} );
-
-gulp.task( 'sass', function() {
-	return gulp.src( 'assets/sass/*.scss' )
-		.pipe( gp_sass( sass_options ) )
-		.pipe( gp_rename( 'style.css' ) )
-		.pipe( gulp.dest( './' ) );
-} );
-
-gulp.task( 'cssmin', function() {
-	return gulp.src( 'style.css' )
-		.pipe( gp_cssmin() )
-		.pipe( gp_rename( 'style.min.css' ) )
-		.pipe( gulp.dest( './' ) );
 } );
 
 gulp.task( 'watch', function() {
@@ -51,4 +27,4 @@ gulp.task( 'build', function() {
 	], { base: "." } ).pipe( gulp.dest( 'woominecraft' ) );
 } );
 
-gulp.task( 'default', gulp.series( 'scripts', 'sass', 'cssmin' ) );
+gulp.task( 'default', gulp.series( 'scripts' ) );
